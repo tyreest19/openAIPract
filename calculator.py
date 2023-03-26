7029bf30ec97d1ee4bf0aaf5161e899be6cb1662
tyreest19 <tyreeostevenson@gmail.com>
2023-03-25 23:26:35 -0400

    I made the calculator a class

--- calculator.py
+++ calculator.py
@@ -1,12 +1,16 @@
 """
 Calculator library containing basic math operations.
 """
+class Calculator():
 
-def add(first_term, second_term):
-    return first_term + second_term
+    def __init__(self):
+        pass
 
-def subtract(first_term, second_term):
-    return first_term - second_term
+    def add(self, first_term, second_term):
+        return first_term + second_term
 
-def divide(first_term, second_term):
-    return (first_term/second_term)
+    def subtract(self,first_term, second_term):
+        return first_term - second_term
+
+    def divide(self, first_term, second_term):
+        return (first_term/second_term)
--- test_calculator.py
+++ test_calculator.py
@@ -2,16 +2,16 @@
 Unit tests for the calculator library
 """
 
-import calculator
+from calculator import Calculator
 
 
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+        assert 4 == Calculator().add(2, 2)
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        assert 2 == Calculator().subtract(4, 2)
 
     def test_divide(self):
-        assert 2 == calculator.subtract(4, 2)
+        assert 2 == Calculator().divide(4, 2)
